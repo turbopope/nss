ca56886ec6630942cc5e75258bbf652c8f62d175
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2016-07-21 05:47:07 +0200

    Add simulator thingy

---
+++ simulator/Source/Main.hx
@@ -0,0 +1,137 @@
+import haxe.Timer;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.events.Event.RESIZE;
+import openfl.events.Event.ENTER_FRAME;
+import openfl.events.MouseEvent.CLICK;
+import openfl.events.KeyboardEvent.KEY_DOWN;
+import openfl.ui.Keyboard;
+
+
+class Main extends Sprite
+{
+    static inline var WIDTH  = 1920.0;
+    static inline var HEIGHT = 1080.0;
+
+    static inline var NODES  = 200;
+    static inline var MIN_K  = 1;
+    static inline var MAX_K  = 5;
+
+    static inline var MIN_IMPULSE_COUNT = 10;
+    static inline var MAX_IMPULSE_COUNT = 20;
+
+    var content = new Sprite();
+    var nodes   = new Array<Node>();
+    var time    = 0.0;
+    var scale   = 0.0;
+    var step    = 0.0;
+
+    public function new()
+    {
+        super();
+        addChild(content);
+
+        for (i in 0 ... NODES) {
+            var node = new Node(Math.random(), false);
+            node.x   = WIDTH  * Math.random();
+            node.y   = HEIGHT * Math.random();
+            content.addChild(node);
+            nodes.push(node);
+
+            node.buttonMode = true;
+            node.addEventListener(CLICK, function (_) {
+                node.state = !node.state;
+            });
+        }
+
+        for (node in nodes) {
+            var nodesLeft = nodes.copy();
+            nodesLeft.remove(node);
+            for (n in node.neighbors) {
+                nodesLeft.remove(n);
+            }
+
+            var k = rand(MIN_K, MAX_K);
+            for (i in 0 ... k) {
+                var neighbor = nodesLeft[Std.random(nodesLeft.length)];
+                nodesLeft.remove(neighbor);
+                node.addNeighbor(neighbor);
+                neighbor.addNeighbor(node);
+            }
+
+            content.addChildAt(node.lines, 0);
+        }
+
+        stage.addEventListener(KEY_DOWN, onKey);
+
+        stage.addEventListener(RESIZE, resize);
+        resize();
+
+        addEventListener(ENTER_FRAME, onFrame);
+    }
+
+
+    function onKey(e)
+    {
+        switch (e.keyCode) {
+            case Keyboard.S: {
+                ++step;
+            }
+            case Keyboard.C: {
+                for (n in nodes) {
+                    n.checkState();
+                }
+            }
+            case Keyboard.I: {
+                impulse();
+            }
+            case Keyboard.UP: {
+                scale += 0.01;
+            }
+            case Keyboard.DOWN: {
+                scale = Math.max(0, scale - 0.01);
+            }
+            case Keyboard.SPACE: {
+                scale = 0;
+            }
+        }
+    }
+
+
+    function onFrame(_)
+    {
+        var prev = Std.int(time);
+        time += scale + step;
+        step  = 0;
+
+        var t = Std.int(time) - prev;
+        for (n in nodes) {
+            n.step(t);
+        }
+    }
+
+
+    function impulse()
+    {
+        var impulses  = rand(MIN_IMPULSE_COUNT, MAX_IMPULSE_COUNT);
+        var nodesLeft = nodes.copy();
+        for (i in 0 ... impulses) {
+            var node = nodesLeft[Std.random(nodesLeft.length)];
+            nodesLeft.remove(node);
+            node.state = true;
+        }
+    }
+
+
+    function resize(?e:Event)
+    {
+        content.scaleX = stage.stageWidth  / WIDTH;
+        content.scaleY = stage.stageHeight / HEIGHT;
+    }
+
+
+    public static function rand(min:Int, max:Int)
+    {
+        return Std.random(max - min + 1) + min;
+    }
+}
---
+++ simulator/Source/Node.hx
@@ -0,0 +1,100 @@
+import Main.rand;
+import haxe.Timer;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.display.Sprite;
+
+using Lambda;
+
+
+class Node extends Sprite
+{
+    static inline var MIN_DELAY = 1;
+    static inline var MAX_DELAY = 30;
+
+    static var blue:BitmapData;
+    static var red :BitmapData;
+
+    public var tau      (default, null):Float;
+    public var state    (default, set ):Bool;
+    public var lines    (default, null):Sprite;
+    public var neighbors(default, null) = new Array<Node>();
+
+    var check    = 0;
+    var checking = false;
+    var oldState = false;
+    var bitmap:Bitmap;
+
+    public function new(tau:Float, state:Bool)
+    {
+        super();
+
+        if (blue == null) {
+            blue = new BitmapData(8, 8, false, 0x0000ff);
+        }
+        if (red == null) {
+            red = new BitmapData(8, 8, false, 0xff0000);
+        }
+
+        bitmap = new Bitmap(blue);
+        bitmap.x = bitmap.width  / -2;
+        bitmap.y = bitmap.height / -2;
+        addChild(bitmap);
+
+        this.tau   = tau;
+        this.state = state;
+        this.lines = new Sprite();
+
+        lines.graphics.lineStyle(1, 0);
+        lines.alpha = 0.1;
+    }
+
+
+    function set_state(s:Bool):Bool
+    {
+        state = s;
+        var bmp = state ? red : blue;
+        if (bmp != bitmap.bitmapData) {
+            bitmap.bitmapData = bmp;
+        }
+        return state;
+    }
+
+    public function checkState()
+    {
+        if (checking) {
+            lines.alpha = 0.5;
+            scaleX = scaleY = 2;
+
+            var active = neighbors.count(function (n) { return n.state; });
+            state      = active / neighbors.length >= tau;
+            rotation   = state == oldState ? 0 : 45;
+        }
+    }
+
+
+    public function addNeighbor(n:Node)
+    {
+        neighbors.push(n);
+        lines.graphics.moveTo(x, y);
+        lines.graphics.lineTo(n.x, n.y);
+    }
+
+
+    public function step(t:Int)
+    {
+        check -= t;
+        if (check <= 0) {
+            checking = true;
+            oldState = state;
+            checkState();
+            check = rand(MIN_DELAY, MAX_DELAY) + check;
+        }
+        else if (t > 0 && checking) {
+            checking = false;
+            lines.alpha = 0.1;
+            scaleX = scaleY = 1;
+            rotation = 0;
+        }
+    }
+}
