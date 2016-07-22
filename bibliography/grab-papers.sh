f3f102b56d7593887d1451629044a40c28d8477f
Marco Brack <mbrack@uni-koblenz.de>
2016-07-23 00:04:04 +0200

    Add karimi2013temporal

--- bibliography/grab-papers.sh
+++ bibliography/grab-papers.sh
@@ -2,3 +2,6 @@
 
 wget -O papers/watts2002simple.pdf http://www.stat.berkeley.edu/~aldous/260-FMIE/Papers/watts.pdf
 wget -O papers/newman2003structure.pdf http://epubs.siam.org/doi/pdf/10.1137/S003614450342480
+
+echo "Download manually:"
+echo "karimi2013temporal: https://link.springer.com/chapter/10.1007/978-3-642-36461-7_16"
