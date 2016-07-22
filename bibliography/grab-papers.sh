342231628e5b0a8d5316e9f78876e403e03a935b
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2016-07-23 00:01:00 +0200

    Fix paper grabbery and add Milgramery

--- bibliography/grab-papers.sh
+++ bibliography/grab-papers.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-
-wget -O papers/watts2002simple.pdf http://www.stat.berkeley.edu/~aldous/260-FMIE/Papers/watts.pdf
-wget -O papers/newman2003structure.pdf http://epubs.siam.org/doi/pdf/10.1137/S003614450342480
+#!/usr/bin/perl
+use List::Util qw(pairs);
+@PAPERS = qw(
+    watts2002simple         http://www.stat.berkeley.edu/~aldous/260-FMIE/Papers/watts.pdf
+    newman2003structure     http://epubs.siam.org/doi/pdf/10.1137/S003614450342480
+    milgram1969note         https://www.dropbox.com/s/ij30vedzdinljpo/milgram1969note.pdf?raw=1
+);
+for (pairs @PAPERS) {
+    ($name, $url) = @$_;
+    $file = "papers/$name.pdf";
+    system 'wget', '-O', $file, $url unless -e $file;
+}
