8e9a73c495475bf54e67ad28a5c3daf263dabe86
Marco Brack <mbrack@uni-koblenz.de>
2016-07-25 16:25:01 +0200

    Start with step -1.0

--- simulator/Source/Main.hx
+++ simulator/Source/Main.hx
@@ -27,7 +27,7 @@ class Main extends Sprite
     var nodes   = new Array<Node>();
     var time    = 0.0;
     var scale   = 0.0;
-    var step    = 0.0;
+    var step    = -1.0;
 
     public function new()
     {
